172b2c34a4b9ce82383dbeb5ce20fd8275e99a3d
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-23 14:51:00 -0500

    fail well 2sides sum never greater than 3rd spec
    Co-authored-by: Olha Hizhytska <olgainfotech@gmail.com>
    Co-authored-by: Salim Mayan <mailsalim@gmail.com>

--- TriangleTracker.Tests/ModelTests/TriangleTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -7,18 +7,28 @@ namespace TriangleTracker.Tests
   [TestClass]
   public class TriangleTests
   {
+
     [TestMethod]
-    public void Triangle_testTriangle_True()
+    public void Triangle_MakesInstanceOfTriangleWithThreeSideLengths_True()
     {
       Triangle testTriangle = new Triangle(2,3,5);
-      Assert.AreEqual(true, testTriangle.IsTriangle());
+      Assert.AreEqual(2, testTriangle.Side1);
+      Assert.AreEqual(3, testTriangle.Side2);
+      Assert.AreEqual(5, testTriangle.Side3);
     }
 
     [TestMethod]
-    public void Triangle_testConstructor_True()
+    public void IsTriangle_ReturnsTrueWhenAnySumOfTwoSidesIsGreaterThanThird_True()
     {
-      Triangle testTriangle = new Triangle(2,3,5);
-      Assert.AreEqual(3, testTriangle.Side1);
+      Triangle testTriangle = new Triangle(2,3,12);
+      Assert.AreEqual(true, testTriangle.IsTriangle());
     }
   }
 }
+
+// [TestMethod]
+// public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+// {
+//   // any necessary logic to prep for test; instantiating new classes, etc.
+//   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+// }
